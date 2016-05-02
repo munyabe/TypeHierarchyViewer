2d3deeb3854ad9402bcd9c7005a8f2d011913187
munyabe <yo.betek@gmail.com>
2016-05-02 12:43:36 +0900

    Show a target type.

---
+++ TypeHierarchyViewer/Views/Converters/TypeNameConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Microsoft.CodeAnalysis;
+
+namespace TypeHierarchyViewer.Views.Converters
+{
+    /// <summary>
+    /// 型のシンボルを型名に変換するコンバーターです。
+    /// </summary>
+    public class TypeNameConverter : IValueConverter
+    {
+        /// <summary>
+        /// シングルトンのインスタンスです。
+        /// </summary>
+        public static readonly TypeNameConverter Instance = new TypeNameConverter();
+
+        /// <summary>
+        /// インスタンスを初期化します。
+        /// </summary>
+        private TypeNameConverter()
+        {
+        }
+
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var symbol = value as INamedTypeSymbol;
+            return symbol != null ? symbol.ToString() : string.Empty;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
